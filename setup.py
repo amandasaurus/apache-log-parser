bc16172b83e8efc0b19dc8e41c4c580b6c187c17
Rory McCann <rory@technomancy.org>
2014-01-05 13:10:41 +0000

    Python 3.3 support
    (turns out didn't have to do anything, all tests pass)

--- setup.py
+++ setup.py
@@ -3,14 +3,27 @@
 from setuptools import setup
 
 setup(name="apache-log-parser",
-      version="1.2.0",
+      version="1.3.0",
       author="Rory McCann",
       author_email="rory@technomancy.org",
       packages=['apache_log_parser'],
       install_requires = [
         'user-agents',
       ],
-      license = 'GPLv3',
+      license = 'GPLv3+',
       description = "Parse lines from an apache log file",
       test_suite='apache_log_parser.tests',
+      classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+      ],
 )
